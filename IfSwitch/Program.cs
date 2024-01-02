5e957f8fb3b87476247b2feab8ba4455f1e8c0f3
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-02 04:02:40 -0600

    Learned switch statement

--- IfSwitch/Program.cs
+++ IfSwitch/Program.cs
@@ -3,14 +3,14 @@
 // If Else Statement
 // ==, >=, <=, <, >, !=, &&, ||
 
-using System.ComponentModel.Design;
+//using System.ComponentModel.Design;
 
-Console.Write("Entry your Age: ");
-string? age = Console.ReadLine();
+//Console.Write("Entry your Age: ");
+//string? age = Console.ReadLine();
 
-int ageIs = int.Parse(age);
+//int ageIs = int.Parse(age);
 
-if (ageIs >= 30)
+/*if (ageIs >= 30)
 {
     Console.WriteLine("Get Ready to DO WORK!!");
 }
@@ -22,3 +22,37 @@ else
 {
     Console.WriteLine("Too young!");
 }
+*/
+
+
+// Switch Statement
+
+string firstName = "Tim";
+int age = 43;
+
+switch (age)
+{
+    case >= 0 and < 20:
+        Console.WriteLine("You are Young!");
+        break;
+    case >=20 and < 30:
+        Console.WriteLine("Time to do work!");
+        break;
+    default: Console.WriteLine("huhhhh");
+        break;
+}
+
+//switch (firstName.ToLower())
+//{
+//    case "tim" or "sue":
+//        Console.WriteLine("Hello Professor");
+//        break;
+//    case "tom":
+//        Console.WriteLine("Hello tom");
+//        break;
+//    default: 
+//        Console.WriteLine("I don't know you");
+//        break;
+//}
+
+
