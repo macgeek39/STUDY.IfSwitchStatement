140c0217b4252898072f9d473c239703ad58e8d9
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-02 02:50:46 -0600

    Learned Advanced If Statement

--- IfSwitch/Program.cs
+++ IfSwitch/Program.cs
@@ -1,2 +1,23 @@
 ﻿
 
+// If Else Statement
+
+using System.ComponentModel.Design;
+
+Console.Write("Entry your Age: ");
+string? age = Console.ReadLine();
+
+int ageIs = int.Parse(age);
+
+if (ageIs >= 30)
+{
+    Console.WriteLine("Get Ready to DO WORK!!");
+}
+else if ( 30 > ageIs && ageIs >= 20)
+{
+    Console.WriteLine("Time to Party.");
+}
+else
+{
+    Console.WriteLine("Too young!");
+}
